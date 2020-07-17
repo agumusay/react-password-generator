b48f8cb110382f13af38eee628351e0e02a9bd68
agumusay <agumusay@protonmail.com>
2020-07-18 00:20:23 +0200

    build

---
+++ build/precache-manifest.648f9f18fc2a83feea33f479092eb8ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4b6c25a7f93684c513e253aed4d061",
+    "url": "/index.html"
+  },
+  {
+    "revision": "806a1b7d5df19d44c3ca",
+    "url": "/static/css/main.3f975d65.chunk.css"
+  },
+  {
+    "revision": "97f8bee701b676e589df",
+    "url": "/static/js/2.a0a2b15e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0a2b15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806a1b7d5df19d44c3ca",
+    "url": "/static/js/main.3d85c8cd.chunk.js"
+  },
+  {
+    "revision": "ea7cee67bfe696615ef9",
+    "url": "/static/js/runtime-main.4c6c3c1c.js"
+  }
+]);
